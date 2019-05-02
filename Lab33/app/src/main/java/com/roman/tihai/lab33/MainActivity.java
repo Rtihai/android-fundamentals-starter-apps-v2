5c269a991c1ff7426a310990b38dc703bad92b9e
Rtihai <rtihai@gmail.com>
2019-05-02 11:00:05 -0700

    Lab 3.3 - Challenge

---
+++ Lab33/app/src/main/java/com/roman/tihai/lab33/MainActivity.java
@@ -0,0 +1,51 @@
+package com.roman.tihai.lab33;
+
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mHelloTextView;
+    private String[] mColorArray = {"red", "pink", "purple", "deep_purple",
+            "indigo", "blue", "light_blue", "cyan", "teal", "green",
+            "light_green", "lime", "yellow", "amber", "orange", "deep_orange",
+            "brown", "grey", "blue_grey", "black" };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mHelloTextView = findViewById(R.id.hello_textview);
+        // restore saved instance state (the text color)
+        if (savedInstanceState != null) {
+            mHelloTextView.setTextColor(savedInstanceState.getInt("color"));
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // save the current text color
+        outState.putInt("color", mHelloTextView.getCurrentTextColor());
+    }
+
+    public void changeColor(View view) {
+        Random random = new Random();
+        String colorName = mColorArray[random.nextInt(20)];
+        int colorResourceName = getResources().getIdentifier(colorName, "color", getApplicationContext().getPackageName());
+
+        if(Build.VERSION.SDK_INT > 23) {
+            int colorRes = ContextCompat.getColor(this, colorResourceName);
+            mHelloTextView.setTextColor(colorRes);
+        } else {
+            Toast.makeText(this, "Buy a new phone", Toast.LENGTH_LONG).show();
+        }
+    }
+}
